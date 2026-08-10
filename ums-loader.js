2494c12d0b20bb367b599191a0737bd50d19bd6d
ums <cambrethgroup@gmail.com>
2026-08-10 19:20:35 +0530

    Delete ums-loader.js

--- ums-loader.js
+++
@@ -1,9 +0,0 @@
-/* UMS LIVE opening screen: intentional 3.5 second branded loading interval. */
-(() => {
-  const loader = document.querySelector('.ums-opening-screen');
-  if (!loader) return;
-  window.setTimeout(() => {
-    loader.classList.add('is-done');
-    window.setTimeout(() => loader.remove(), 300);
-  }, 3500);
-})();
